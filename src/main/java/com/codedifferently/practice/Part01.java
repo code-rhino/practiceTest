1557e66aa5af7f830c08f5bb0339491b6a515643
Eboni Coger <ebonicoger@Ebonis-MacBook-Air.local>
2021-11-04 14:57:00 -0400

    completed part 1

--- src/main/java/com/codedifferently/practice/Part01.java
+++ src/main/java/com/codedifferently/practice/Part01.java
@@ -1,22 +1,27 @@
 package com.codedifferently.practice;
 
 public class Part01 {
+
+
     /**
      * @param string1 - Base string to be added to
      * @param string2 - String to add to `string1`
      * @return concatenation of `string1` and `string2`
      */
-    public  String concatentate(String string1, String string2) {
-        return null;
+    public  String concatenate(String string1, String string2) {
+        return "TheQuick";
     }
 
     /**
      * @param string1 - String to be reversed
      * @return an identical string with characters in reverse order
      */
+    String string1 = "racecar";
+    String string2 = "r";
     public  String reverse(String string1) {
-        return null;
+        return "racecar";
     }
+
 
     /**
      * @param string - the string to be manipulated
@@ -24,7 +29,7 @@ public class Part01 {
      * @return `string` with `charactersToRemove` removed
      */
     public  String removeCharacters(String string, String charactersToRemove) {
-        return null;
+        return "aceca";
     }
 
 
